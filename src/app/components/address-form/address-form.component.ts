36d3eed447732f1c52a09d34ad2e8d2bdc7ebb1e
wertalp <patrick.wertal@gmail.com>
2017-10-09 16:30:45 +0200

    add DS

--- src/app/components/address-form/address-form.component.ts
+++ src/app/components/address-form/address-form.component.ts
@@ -31,6 +31,7 @@ export class AddressFormComponent implements OnInit {
       new Customer("Bit-Coin-222-ref-xx-AUT-23332---23333",1222,"Patrick","Wertal","patty@gmail.com","+41 79886776766","DAS IST MEIN ANLIEGEN FÜR ZUKUNFT Patrick Wertal"),
       new Customer("Bit-Coin-333-ref-xx-GER-23332---23333",1444,"Thomas","Gilbert","thommy@gmail.com","+41 884448844884","DAS IST MEIN ANLIEGEN FÜR ZUKUNFT  Thomas see you"),
       new Customer("Bit-Coin-666-ref-xx-BRD-23332---23333",1232,"Christoph","P","chriss234@gmail.com","+43 79886776766","DAS IST MEIN ANLIEGEN FÜR ZUKUNFT  Chrissi"),
+      new Customer("Bit-Coin-222-ref-xx-ZSA-23332---23333",1222,"Patrizia","Martini","patrizia@gmail.com","+41 79886776766","DAS IST MEIN ANLIEGEN FÜR ZUKUNFT Patrick Wertal"),
       new Customer("Bit-Coin-888-ref-xx-ITA-23332---23333",12212,"Helge","Volker","helge@gmail.com","+41 79886776766","DAS IST MEIN ANLIEGEN FÜR ZUKUNFT Helge wollte das nur anbringen"),
       new Customer("Bit-Coin-111-ref-xx-SUI-23332---23333",1222,"Danny","Peters","patty@gmail.com","+41 79886776766","DAS IST MEIN ANLIEGEN FÜR ZUKUNFT das gilt auch für ev weitere.."),
       new Customer("Bit-Coin-222-ref-xx-FRA-23332---23333",1222,"Tamara","Oblasser","tammy@gmail.com","+41 79886776766","DAS IST MEIN ANLIEGEN FÜR ZUKUNFT deien Tamara")]
